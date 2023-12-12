ce1f75376b45e908610035ae0e7b01b6ef675948
VladimirVolvakov <dr.volvakov@gmail.com>
2023-12-13 00:43:17 +0300

    feat: install date-fns library, add helper functions & implement price formatting in CabinRow component

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { formatCurrency } from "../../utils/helpers";
 
 const TableRow = styled.div`
   display: grid;
@@ -40,16 +41,15 @@ const Discount = styled.div`
 `;
 
 const CabinRow = ({ cabin }) => {
-  const { image, name, maxCapacity, regularPrice, discount } =
-    cabin;
+  const { image, name, maxCapacity, regularPrice, discount } = cabin;
 
   return (
     <TableRow role="row">
       <Img src={image} />
       <Cabin>{name}</Cabin>
       <div>Fits up to {maxCapacity} guests</div>
-      <Price>{regularPrice}</Price>
-      <Discount>{discount}</Discount>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{formatCurrency(discount)}</Discount>
       <button>Delete</button>
     </TableRow>
   );

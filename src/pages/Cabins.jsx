7106079b7915f69f689b9717ddd89bd0924a051a
VladimirVolvakov <dr.volvakov@gmail.com>
2023-12-13 00:09:53 +0300

    feat: add CabinTable & CabinRow components, implement cabin data fetching using React Query & rendering in Cabin component

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,19 @@
 import Row from "../ui/Row";
 import Heading from "../ui/Heading";
+import CabinTable from "../features/cabins/CabinTable";
 
 const Cabins = () => {
   return (
+    <>
     <Row type="horizontal">
       <Heading as="h1">Cabins</Heading>
+      <p>Filter / Sort</p>
     </Row>
+    <Row>
+      <CabinTable />
+    </Row>
+    </>
+    
   );
 };
 

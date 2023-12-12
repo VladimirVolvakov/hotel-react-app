3bf052d1f38843036d662d86bef035ebab6ea2dd
VladimirVolvakov <dr.volvakov@gmail.com>
2023-12-12 23:15:53 +0300

    feat: set up react-query & create query client for app

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,12 +1,12 @@
-import { useEffect } from "react";
-import { getCabins } from "../services/apiCabins";
+import Row from "../ui/Row";
+import Heading from "../ui/Heading";
 
 const Cabins = () => {
-  useEffect(() => {
-    getCabins().then(data => console.log(data));
-  }, []);
-
-  return <div>Cabins</div>;
+  return (
+    <Row type="horizontal">
+      <Heading as="h1">Cabins</Heading>
+    </Row>
+  );
 };
 
 export default Cabins;

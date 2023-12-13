5d17d63b6abfae5bfe2da88b967e32ee0b15a4db
VladimirVolvakov <dr.volvakov@gmail.com>
2023-12-13 23:49:28 +0300

    feat: add CreateCabinForm component to Cabins page

---
+++ src/features/cabins/CreateCabinForm.jsx
@@ -0,0 +1,136 @@
+import styled, { css } from "styled-components";
+import Input from "../../ui/Input";
+import Button from "../../ui/Button";
+
+const Form = styled.form`
+  ${(props) =>
+    props.type !== "modal" &&
+    css`
+      padding: 2.4rem 4rem;
+      background-color: var(--color-grey-0);
+      border: 1px solid var(--color-grey-100);
+      border-radius: var(--border-radius-md);
+    `}
+
+  ${(props) =>
+    props.type === "modal" &&
+    css`
+      width: 80rem;
+    `}
+    
+  overflow: hidden;
+  font-size: 1.4rem;
+`;
+
+const FormRow = styled.div`
+  display: grid;
+  align-items: center;
+  grid-template-columns: 24rem 1fr 1.2fr;
+  gap: 2.4rem;
+
+  padding: 1.2rem 0;
+
+  &:first-child {
+    padding-top: 0;
+  }
+
+  &:last-child {
+    padding-bottom: 0;
+  }
+
+  &:not(:last-child) {
+    border-bottom: 1px solid var(--color-grey-100);
+  }
+
+  &:has(button) {
+    display: flex;
+    justify-content: flex-end;
+    gap: 1.2rem;
+  }
+`;
+
+const Label = styled.label`
+  font-weight: 500;
+`;
+
+const Error = styled.span`
+  font-size: 1.4rem;
+  color: var(--color-red-700);
+`;
+
+const FileInput = styled.input`
+  font-size: 1.4rem;
+  border-radius: var(--border-radius-sm);
+
+  &::file-selector-button {
+    font: inherit;
+    font-weight: 500;
+    padding: 0.8rem 1.2rem;
+    margin-right: 1.2rem;
+    border-radius: var(--border-radius-sm);
+    border: none;
+    color: var(--color-brand-50);
+    background-color: var(--color-brand-600);
+    cursor: pointer;
+    transition: color 0.2s, background-color 0.2s;
+
+    &:hover {
+      background-color: var(--color-brand-700);
+    }
+  }
+`;
+
+const Textarea = styled.textarea`
+  padding: 0.8rem 1.2rem;
+  border: 1px solid var(--color-grey-300);
+  border-radius: 5px;
+  background-color: var(--color-grey-0);
+  box-shadow: var(--shadow-sm);
+  width: 100%;
+  height: 8rem;
+`;
+
+const CreateCabinForm = () => {
+  return (
+    <Form>
+      <FormRow>
+        <Label htmlFor="name">Cabin name</Label>
+        <Input type="text" id="name" />
+      </FormRow>
+
+      <FormRow>
+        <Label htmlFor="maxCapacity">Maximum capacity</Label>
+        <Input type="number" id="maxCapacity" />
+      </FormRow>
+
+      <FormRow>
+        <Label htmlFor="regularPrice">Regular price</Label>
+        <Input type="number" id="regularPrice" />
+      </FormRow>
+
+      <FormRow>
+        <Label htmlFor="discount">Discount</Label>
+        <Input type="number" id="discount" defaultValue={0} />
+      </FormRow>
+
+      <FormRow>
+        <Label htmlFor="description">Description for website</Label>
+        <Textarea type="number" id="description" defaultValue="" />
+      </FormRow>
+
+      <FormRow>
+        <Label htmlFor="image">Cabin photo</Label>
+        <FileInput id="image" accept="image/*" />
+      </FormRow>
+
+      <FormRow>
+        <Button variation="secondary" type="reset">
+          Cancel
+        </Button>
+        <Button>Edit cabin</Button>
+      </FormRow>
+    </Form>
+  );
+};
+
+export default CreateCabinForm;
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,19 +1,29 @@
 import Row from "../ui/Row";
 import Heading from "../ui/Heading";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import { useState } from "react";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 const Cabins = () => {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <>
-    <Row type="horizontal">
-      <Heading as="h1">Cabins</Heading>
-      <p>Filter / Sort</p>
-    </Row>
-    <Row>
-      <CabinTable />
-    </Row>
+      <Row type="horizontal">
+        <Heading as="h1">Cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+      <Row>
+        <CabinTable />
+
+        <Button onClick={() => setShowForm((curState) => !curState)}>
+          Add new cabin
+        </Button>
+
+        { showForm && <CreateCabinForm /> }
+      </Row>
     </>
-    
   );
 };
 

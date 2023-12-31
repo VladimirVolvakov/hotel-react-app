dc8994b10e663f0f5ea6534ec86babb344895611
VladimirVolvakov <dr.volvakov@gmail.com>
2023-12-31 12:08:01 +0300

    feat: implement bookings filter functionality, add TableOperations to separate component

--- src/features/bookings/useFetchBookingsData.js
+++ src/features/bookings/useFetchBookingsData.js
@@ -1,10 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router-dom";
 import { getBookings } from "../../services/apiBookings";
 
 export const useFetchBookingsData = () => {
+  const [searchParams] = useSearchParams();
+
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : {
+          field: "status",
+          value: filterValue,
+        };
+
   const res = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter }),
   });
 
   const { data: bookings, isLoading, error } = res;
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,9 +1,16 @@
 import supabase from "./supabase";
 
-export const getBookings = async () => {
-  const { data: bookings, error } = await supabase
+export const getBookings = async ({ filter, sortBy }) => {
+  let query = supabase
     .from("bookings")
     .select("*, cabins(name), guests(fullName, email)");
+
+  if (filter !== null) {
+    const { field, value, method } = filter;
+    query = query[method || "eq"](field, value);
+  }
+
+  const { data: bookings, error } = await query;
 
   if (error) {
     console.error(error.message);

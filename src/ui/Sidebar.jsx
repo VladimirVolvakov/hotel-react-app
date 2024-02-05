8cfd4ae7e2488f80778a733ea5ab8578816120c7
VladimirVolvakov <dr.volvakov@gmail.com>
2024-02-05 14:50:35 +0300

    feat: made Uploader component visible on the sidebar

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-// import Uploader from "../data/Uploader";
+import Uploader from "../data/Uploader";
 import Logo from "./Logo";
 import MainNav from "./MainNav";
 
@@ -20,7 +20,7 @@ const Sidebar = () => {
       <MainNav />
 
       {/* This component was used to generate fake data for app: */}
-      {/* <Uploader /> */}
+      <Uploader />
     </StyledSidebar>
   );
 };
